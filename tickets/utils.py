21b707d37222d281f664a9ac4a56fe855b7cd5ee
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2025-08-03 22:14:23 +0000

    Update utils.py

--- tickets/utils.py
+++ tickets/utils.py
@@ -3,8 +3,10 @@ from io import BytesIO
 from django.core.files.base import ContentFile
 
 def generate_qr_code(ticket):
-    data = f"https://ticketing-production-20dc.up.railway.app//valider-ticket/{ticket.id}/"  # adapte le lien
+    data = f"https://ticketing-production-20dc.up.railway.app/valider-ticket/{ticket.id}/"  # adapte le lien
+    print(f"Generating QR code for data: {data}")
     qr = qrcode.make(data)
     buffer = BytesIO()
     qr.save(buffer, format='PNG')
+    print(f"QR code generated for ticket {ticket.id}, size: {buffer.tell()} bytes")
     return ContentFile(buffer.getvalue(), name=f"qr_ticket_{ticket.id}.png")
