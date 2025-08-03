2f0159bd89edcf4bec4622dae666e8265962fbc5
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2025-08-03 21:07:19 +0000

    Update pdf_utils.py

--- tickets/pdf_utils.py
+++ tickets/pdf_utils.py
@@ -2,12 +2,18 @@ from django.template.loader import render_to_string
 from xhtml2pdf import pisa
 from io import BytesIO
 import base64
+import requests
 
 def generate_ticket_pdf(ticket):
-    # Lire le QR code en base64 pour l'inclure dans le HTML
-    # Lire le contenu du fichier depuis Cloudinary (ou local)
-    qr_file = ticket.qr_code
-    encoded_string = base64.b64encode(qr_file.read()).decode()
+    # Lire le QR code depuis Cloudinary via son URL
+    try:
+        response = requests.get(ticket.qr_code.url)
+        response.raise_for_status()
+    except Exception as e:
+        print("Erreur lors du téléchargement du QR code :", e)
+        return None
+
+    encoded_string = base64.b64encode(response.content).decode()
 
     html_string = render_to_string('tickets/ticket_pdf.html', {
         'ticket': ticket,
@@ -18,4 +24,3 @@ def generate_ticket_pdf(ticket):
     if pisa_status.err:
         return None
     return pdf_file.getvalue()
-
