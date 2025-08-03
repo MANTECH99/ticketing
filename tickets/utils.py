f0b29aa67c26c84a610994e25d5318f5bc780240
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2025-08-03 19:52:12 +0000

    Update utils.py

--- tickets/utils.py
+++ tickets/utils.py
@@ -3,8 +3,12 @@ from io import BytesIO
 from django.core.files.base import ContentFile
 
 def generate_qr_code(ticket):
-    data = f"https://19dacf9b9c22.ngrok-free.app/valider-ticket/{ticket.id}/"  # adapte le lien
+    data = f"https://ticketing-production-20dc.up.railway.app//valider-ticket/{ticket.id}/"  # adapte le lien
     qr = qrcode.make(data)
     buffer = BytesIO()
     qr.save(buffer, format='PNG')
-    return ContentFile(buffer.getvalue(), name=f"qr_ticket_{ticket.id}.png")
+    file = ContentFile(buffer.getvalue(), name=f"qr_ticket_{ticket.id}.png")
+
+    # Sauvegarde dans le champ qr_code → envoie vers Cloudinary
+    ticket.qr_code.save(file.name, file)
+    ticket.save()
