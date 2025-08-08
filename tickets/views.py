28a0c57bd137adfdc20d7677a77c6e583f1ac507
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2025-08-08 21:50:30 +0000

    Update views.py

--- tickets/views.py
+++ tickets/views.py
@@ -6,7 +6,7 @@ from events.models import Ticket
 @login_required
 def telecharger_ticket(request, ticket_id):
     ticket = Ticket.objects.get(id=ticket_id, reservation__user=request.user)
-    pdf_content = generate_ticket_pdf(ticket)
+    pdf_content = generate_ticket_pdf(ticket, request)
     if pdf_content is None:
         return HttpResponse("Erreur lors de la génération du PDF.", status=500)
     response = HttpResponse(pdf_content, content_type='application/pdf')
